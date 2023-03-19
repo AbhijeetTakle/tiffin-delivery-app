bbf7971b3f102b672636e23f299177fb10ee3015
Abhijeet Takle <abhijeettakle35@gmail.com>
2023-03-19 22:37:41 +0530

    Implemented signin and signup funtionality
    -Added and connected firebase with the app
    -Added signin screen has text fields for email and password, and a register link which takes user to the signup page
    -User firebase auth instance to signin user with email and password, on successfull login user is navigated to the services list screen
    -On register link click user is navigated to the signup page
    -Signup screen has text fields for username, email password, confirmPassword, and a signup button
    -Used firebase auth instance to create user with email and password, on successfull signup the username is update in the profile and user is navigated to login screen

--- app/src/main/java/com/example/tiffindeliveryapp/LoginFragment.kt
+++ app/src/main/java/com/example/tiffindeliveryapp/LoginFragment.kt
@@ -1,59 +1,74 @@
 package com.example.tiffindeliveryapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var loginUsername:EditText
+    private lateinit var loginPassword:EditText
+    private lateinit var login:Button
+    private lateinit var registerNewUser:TextView
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        loginUsername = view.findViewById(R.id.login_username)
+        loginPassword = view.findViewById(R.id.login_password)
+        login = view.findViewById(R.id.login_btn)
+        registerNewUser = view.findViewById(R.id.register_user_link)
+        mAuth = Firebase.auth
+        setActionToButtons()
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun setActionToButtons() {
+        login.setOnClickListener{
+            val username = loginUsername.text.toString().trim()
+            val password = loginPassword.text.toString().trim()
+            if(checkValidInput()){
+                mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        findNavController().navigate(R.id.action_loginFragment_to_tiffinServicesListFragment)
+                    }else{
+                        Toast.makeText(context,"${it.exception?.message.toString()}", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
+        }
+        registerNewUser.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+        }
+    }
+
+    private fun checkValidInput():Boolean {
+        val username = loginUsername.text.toString().trim()
+        val password = loginPassword.text.toString().trim()
+
+        if(username.isEmpty()){
+            loginUsername.error = "Cannot be empty"
+            loginUsername.requestFocus()
+            return false
+        }
+        if(password.isEmpty()){
+            loginPassword.error = "Cannot be empty"
+            loginPassword.requestFocus()
+            return false
+        }
+        return true
     }
 }
---
+++ app/src/main/java/com/example/tiffindeliveryapp/viewmodels/SplashScreenViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.tiffindeliveryapp.viewmodels
+
+import android.os.Handler
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenViewModel:ViewModel() {
+    val isLoading = MutableLiveData<Boolean>()
+
+    init {
+        isLoading.value = true
+
+        viewModelScope.launch {
+            delay(3000)
+            isLoading.value = false
+        }
+    }
+}
