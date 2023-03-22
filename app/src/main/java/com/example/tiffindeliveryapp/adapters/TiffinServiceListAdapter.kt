3ef175e1e92c45c7a8a6617e38507b9c4d1b4457
Abhijeet Takle <abhijeettakle35@gmail.com>
2023-03-22 15:12:08 +0530

    Implemented Services List screen which shows the list of services available for users to order from
    -Added recylcer view in the ServiceList fragment and implemented the correspnding adapter
    -Fetched data from firestore database and updated the list

---
+++ app/src/main/java/com/example/tiffindeliveryapp/adapters/TiffinServiceListAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.tiffindeliveryapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.tiffindeliveryapp.R
+import com.example.tiffindeliveryapp.datamodels.TiffinService
+
+class TiffinServiceListAdapter(var services:ArrayList<TiffinService>):Adapter<TiffinServiceListAdapter.TiffinServiceViewHolder>() {
+
+    fun onDatasetChanged(list:ArrayList<TiffinService>){
+        services = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TiffinServiceViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.service_list_item, parent, false)
+        return TiffinServiceViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TiffinServiceViewHolder, position: Int) {
+        holder.serviceTitle.text = services[position].title
+        holder.serviceDescription.text = services[position].description
+        holder.serviceSubscriberCount.text = services[position].subscriberCount.toString()+" subscribers"
+        holder.serviceRating.text = services[position].rating.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return services.size
+    }
+
+    class TiffinServiceViewHolder(itemView: View) :ViewHolder(itemView){
+        val serviceTitle:TextView
+        val serviceDescription:TextView
+        val serviceSubscriberCount:TextView
+        val serviceRating:TextView
+        val serviceImage:ImageView
+
+        init {
+            serviceTitle = itemView.findViewById(R.id.food_service_title)
+            serviceDescription = itemView.findViewById(R.id.food_service_description)
+            serviceSubscriberCount = itemView.findViewById(R.id.food_service_subscriber_count)
+            serviceRating = itemView.findViewById(R.id.food_service_rating)
+            serviceImage = itemView.findViewById(R.id.food_service_image)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tiffindeliveryapp/viewmodels/TiffinServicesListViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.tiffindeliveryapp.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tiffindeliveryapp.datamodels.TiffinService
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.*
+
+class TiffinServicesListViewModel:ViewModel() {
+    var servicesList:MutableLiveData<ArrayList<TiffinService>> = MutableLiveData(ArrayList())
+    init {
+        getServicesData()
+    }
+
+    private fun getServicesData() = runBlocking{
+        val list = ArrayList<TiffinService>()
+        viewModelScope.async(Dispatchers.Default) {
+            val db = Firebase.firestore
+            db.collection("TiffinServices")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        val service = document.toObject(TiffinService::class.java)
+                        list.add(service)
+                    }
+                    viewModelScope.async {
+                        withContext(Dispatchers.Main){
+                            servicesList.value = list
+                        }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("TAG", "Error getting documents.", exception)
+                }
+        }
+    }
+}
