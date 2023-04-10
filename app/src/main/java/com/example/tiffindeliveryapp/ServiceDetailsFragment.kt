406b6011fe065b22f1f3471d800ea55e67f0ce76
RagingScout95 <prakhar99rajput@gmail.com>
2023-04-10 12:39:38 +0530

    updated xml

--- app/src/main/java/com/example/tiffindeliveryapp/ServiceDetailsFragment.kt
+++ app/src/main/java/com/example/tiffindeliveryapp/ServiceDetailsFragment.kt
@@ -7,11 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.tiffindeliveryapp.datamodels.TiffinService
 import com.example.tiffindeliveryapp.viewmodelfactory.ServiceDetailsViewModelFactory
 import com.example.tiffindeliveryapp.viewmodels.ServiceDetailsViewModel
+import org.w3c.dom.Text
 import kotlin.math.log
 
 class ServiceDetailsFragment : Fragment() {
@@ -23,6 +25,9 @@ class ServiceDetailsFragment : Fragment() {
     private lateinit var serviceSubscriberCount: TextView
     private lateinit var serviceRating: TextView
     private lateinit var serviceImage: ImageView
+    private lateinit var serviceMenu:TextView
+    private lateinit var serviceTiffinTypes:LinearLayout
+    private lateinit var serviceReviews:LinearLayout
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,6 +38,9 @@ class ServiceDetailsFragment : Fragment() {
         serviceSubscriberCount = view.findViewById(R.id.food_service_subscriber_count)
         serviceRating = view.findViewById(R.id.food_service_rating)
         serviceImage = view.findViewById(R.id.food_service_image)
+        serviceMenu = view.findViewById(R.id.service_menu)
+        serviceTiffinTypes = view.findViewById(R.id.service_tiffin_types)
+        serviceReviews = view.findViewById(R.id.service_reviews)
         arguments?.getString("ServiceID")?.let {
             serviceID = it
         }
@@ -41,18 +49,32 @@ class ServiceDetailsFragment : Fragment() {
             if(!it.isEmpty()){
                 service = it[0]
                 Log.d("TAG", "updateService: "+service.title)
-                updateService()
+                updateService(inflater)
             }
         }
         return view
     }
 
-    private fun updateService() {
+    private fun updateService(inflator:LayoutInflater) {
         serviceTitle.text = service.title
         serviceDescription.text = service.description
         serviceSubscriberCount.text = service.subscriberCount.toString()+" subscribers"
         serviceRating.text = service.rating.toString()
+        serviceMenu.text = service.todaysMenu
+
+        for(s in service.tiffinTypes.keys){
+            val tiffinType = TextView(requireContext())
+            tiffinType.text = service.tiffinTypes.get(s)
+            serviceTiffinTypes.addView(tiffinType)
+        }
+
+        for(s in service.customerReviews.keys){
+            val review = layoutInflater.inflate(R.layout.review_list_item, serviceReviews, false)
+            review.findViewById<TextView>(R.id.review_user).text = s
+            review.findViewById<TextView>(R.id.review_comment).text = service.customerReviews.get(s)
+            serviceReviews.addView(review)
+        }
+
+
     }
-
-
 }
